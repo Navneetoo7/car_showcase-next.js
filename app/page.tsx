70f7d1fae7b159905c60dfa24dcc95cd62d7c84c
navneet <mani@indovations.in>
2023-08-28 00:02:07 +0530

    car and dialog added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Hero, SearchBar,CustomFilter } from "@/components";
-import Image from 'next/image'
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { fetchCars } from "@/utils";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
-    <main className="overflow-hidden">
+    <div className="overflow-hidden">
       <Hero />
       <div
         className="mt-12 padding-x padding-y max-width"
@@ -20,7 +23,18 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section><div className="home__cars-wrapper">
+            {allCars?.map((car) => (
+              <CarCard car={car}/>
+            )) }</div></section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
-    </main>
+    </div>
   );
 }
